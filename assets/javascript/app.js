92745770d680dcd27774e9e9effa5e9864b74854
Anh Lu <anhlucci@outlook.com>
2018-06-11 18:31:46 -0400

    some more changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,7 @@ var questions_answers = [
     },
 
     {   question: "In which country was tea first drunk?",
-        answers: ['Endland', 'India', 'China', 'USA'],
+        answers: ['England', 'India', 'China', 'USA'],
         correctAnswer:"China"
     },
 
@@ -22,42 +22,40 @@ var timer = 10;
 var intervalID;
 
 var generateQuestionSet = function(){
-    rand = Math.floor(Math.random() * questions_answers.length);questionSet = questions_answers[rand];
+    rand = Math.floor(Math.random() * questions_answers.length);
+    questionSet = questions_answers[rand];
 };
 generateQuestionSet();
 
-var timeOut = setTimeout (function(){
-    $("#correctAnswer").html("Correct answer: " + questionSet.correctAnswer);
-}, 10000);
+// var timeOut = setTimeout (function(){
+//     $("#correctAnswer").html("Correct answer: " + questionSet.correctAnswer);
+// }, 10000);
 
 var decrement = function(){
     timer--;
     $("#timer").html(timer);
+    if (timer === 0) {
+        $("#correctAnswer").html("Correct answer: " + questionSet.correctAnswer);
+        stop();
+    }
 };
-
-var stop = function(){
+function stop (){
     clearInterval(intervalID);
 };
 
 var run = function(){
     intervalID = setInterval(decrement, 1000);
-    if (timer === 0){
-        stop()};
     };
-    
-
 
 document.getElementById("startButton").addEventListener('click',function(){
     $(".hide").show();
     $("#startButton").hide();
-    decrement();
     run();
     document.getElementById("question").innerHTML = questionSet.question;
-    // document.getElementById("choices").innerHTML = questionSet.answers.join(' ');
-    $(questionSet.answers).each(function(){
-        $("#choices").html("<button>" + this + "</button>");
-    })
-    timeOut();
+    questionSet.answers.forEach(function(answer) {
+    $("#choices").append("<button>" + answer + "</button>");
+});
+    
 });
 
 
